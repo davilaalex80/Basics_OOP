aa437e0b26cdc85cb52b95b31787fd9fe47b4679
davilaalex80 <156226701+davilaalex80@users.noreply.github.com>
2024-02-19 10:38:40 -0600

    adding first files of the course

---
+++ EntryPoint.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class EntryPoint
+{
+    static void Main()
+    {
+        Point point = new Point(5,3);
+
+        //point.X = 5;
+        //point.Y = 3;
+
+        Point pointTwo = new Point();
+        pointTwo.X = 5;
+
+        Console.WriteLine(point.X);
+        Console.WriteLine(point.Y);
+    }
+}
---
+++ PointAndLines/Point.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroOOP.PointAndLines
+{
+    class Point
+    {
+        private int x;
+        public int y;
+
+        public int X
+        {
+            get { return x; } 
+            set { x = value;}
+        }
+
+        //empty constructor
+        public Point()
+        {
+
+        }
+
+        public Point(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+    }
+}
